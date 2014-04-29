555205766fc66ebf46961e8044d017efe18b3c0c
Tony Boyles <tboyles@sosacorp.com>
2014-04-29 14:31:20 -0400

    Added the structure to include tests, though not any actual tests at this point.

---
+++ tests/tests.js
@@ -0,0 +1,59 @@
+test( "Table2Array", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "Table2Object", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "Table2CSV", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "Table2CSVURL", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "CSV2Table", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "CSV2Array", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "CSV2Object", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "jQcsv2Table", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "jQtable2CSV", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
+
+test( "jQdownloadTable", function() {
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+  ok( 1 == "1", "Passed!" );
+});
